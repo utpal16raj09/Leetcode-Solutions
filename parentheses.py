eb2f3ddf2929ea438a98b7237da348fca7194771
Utpal Raj <134092243+utpal16raj09@users.noreply.github.com>
2023-12-01 00:37:07 +0530

    Add files via upload

---
+++ parentheses.py
@@ -0,0 +1,20 @@
+def isValid(s: str) -> bool:
+    # Stack for left symbols
+    leftSymbols = []
+    # Loop for each character of the string
+    for c in s:
+        # If left symbol is encountered
+        if c in ['(', '{', '[']:
+            leftSymbols.append(c)
+        # If right symbol is encountered
+        elif c == ')' and len(leftSymbols) != 0 and leftSymbols[-1] == '(':
+            leftSymbols.pop()
+        elif c == '}' and len(leftSymbols) != 0 and leftSymbols[-1] == '{':
+            leftSymbols.pop()
+        elif c == ']' and len(leftSymbols) != 0 and leftSymbols[-1] == '[':
+            leftSymbols.pop()
+        # If none of the valid symbols is encountered
+        else:
+            return False
+    # Check if all left symbols are matched
+    return not leftSymbols
